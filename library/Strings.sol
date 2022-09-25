e5fdf1c98861bca4fffcfd7f816ffbbb469a2252
Sam Larsen <sam@slarsen.io>
2022-09-24 23:56:10 -0400

    👷‍♂️ Address string

--- library/Strings.sol
+++ library/Strings.sol
@@ -1,22 +1,56 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
+
+import "./Math.sol";
 
 library Strings {
+    bytes16 private constant _SYMBOLS = "0123456789abcdef";
+    uint8 private constant _ADDRESS_LENGTH = 20;
+
     function toString(uint256 value) internal pure returns (string memory) {
-        if (value == 0) return "0";
-        uint256 temp = value;
-        uint256 digits;
-        while (temp != 0) {
-            digits++;
-            temp /= 10;
+        unchecked {
+            uint256 _ptr;
+            uint256 _length = Math.log10(value) + 1;
+            string memory buffer = new string(_length);
+            assembly {
+                _ptr := add(buffer, add(32, _length))
+            }
+            while (true) {
+                _ptr--;
+                assembly {
+                    mstore8(_ptr, byte(mod(value, 10), _SYMBOLS))
+                }
+                value /= 10;
+                if (value == 0) break;
+            }
+            return buffer;
         }
-        bytes memory buffer = new bytes(digits);
-        while (value != 0) {
-            digits -= 1;
-            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
-            value /= 10;
+    }
+
+    function toHexString(uint256 value) internal pure returns (string memory) {
+        unchecked {
+            return toHexString(value, Math.log256(value) + 1);
         }
+    }
+
+    function toHexString(address addr) internal pure returns (string memory) {
+        return toHexString(uint256(uint160(addr)), _ADDRESS_LENGTH);
+    }
+
+    function toHexString(uint256 value, uint256 length)
+        internal
+        pure
+        returns (string memory)
+    {
+        bytes memory buffer = new bytes(2 * length + 2);
+        buffer[0] = "0";
+        buffer[1] = "x";
+        for (uint256 i = 2 * length + 1; i > 1; --i) {
+            buffer[i] = _SYMBOLS[value & 0xf];
+            value >>= 4;
+        }
+        require(value == 0);
         return string(buffer);
     }
 }
