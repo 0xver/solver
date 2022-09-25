4c30106615e564d0e2aa4951a2dd61f1a6aae78b
Sam Larsen <sam@slarsen.io>
2022-09-24 23:57:55 -0400

    👷‍♂️ Metadata structure

--- token/metadata/ERC721Metadata.sol
+++ token/metadata/ERC721Metadata.sol
@@ -10,16 +10,23 @@ import "../../library/Base64.sol";
 contract ERC721Metadata is ERC721, IERC721Metadata {
     string private _name;
     string private _symbol;
+    string private _description;
     string private _defaultImage;
+    string private _defaultAnimation;
 
     constructor(
         string memory name_,
         string memory symbol_,
-        string memory defaultImage_
+        string memory description_,
+        string memory defaultImage_,
+        // Use "" to void animation_url property
+        string memory defaultAnimation_
     ) {
         _name = name_;
         _symbol = symbol_;
+        _description = description_;
         _defaultImage = defaultImage_;
+        _defaultAnimation = defaultAnimation_;
     }
 
     function name()
@@ -49,25 +56,69 @@ contract ERC721Metadata is ERC721, IERC721Metadata {
         override(IERC721Metadata)
         returns (string memory)
     {
-        bytes memory dataURI = abi.encodePacked(
+        bytes memory _animation;
+        if (
+            keccak256(abi.encodePacked(_defaultAnimation)) !=
+            keccak256(abi.encodePacked(""))
+        ) {
+            _animation = abi.encodePacked(
+                '"animation_url":"',
+                _defaultAnimation,
+                '",'
+            );
+        } else {
+            delete _animation;
+        }
+        bytes memory data = abi.encodePacked(
             "{",
-            '"name":"ERC721 Test #',
+            '"name":"',
+            name(),
+            " #",
             Strings.toString(_tokenId),
             '",',
-            '"description":"ERC721 Test"',
+            '"description":"',
+            _description,
             '",',
             '"image":"',
             _defaultImage,
+            '",',
+            _animation,
+            '"attributes":[',
+            "{",
+            '"trait_type":"Owner",',
+            '"value":"',
+            Strings.toHexString(ownerOf(_tokenId)),
+            '"',
+            "},",
+            "{",
+            '"trait_type":"Balance",',
+            '"value":"',
+            Strings.toString(balanceOf(ownerOf(_tokenId))),
+            '"',
+            "}",
+            "{",
+            '"trait_type":"Approved",',
+            '"value":"',
+            Strings.toHexString(getApproved(_tokenId)),
+            '"',
+            "}",
+            "{",
+            '"trait_type":"Timestamp",',
+            '"value":"',
+            Strings.toString(block.timestamp),
+            '"',
+            "}",
+            "]",
             '"}'
         );
-        if (!_exists(_tokenId)) {
+        if (ownerOf(_tokenId) == address(0)) {
             return "Invalid ID";
         } else {
             return
                 string(
                     abi.encodePacked(
                         "data:application/json;base64,",
-                        Base64.encode(dataURI)
+                        Base64.encode(data)
                     )
                 );
         }
